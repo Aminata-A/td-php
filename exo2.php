bd37f351b4b826cc9ba0d4e338cfd314f72506b3
Aminata-A <aminataassane.ndiaye@gmail.com>
2024-02-21 12:45:40 +0000

    exercice2 refait

--- exo2.php
+++ exo2.php
@@ -1,21 +1,11 @@
 <?php
-//Exercice2:
 function supprimerDoublons($tableau) {
     $tableauSansDoublons = array_unique($tableau);
     return $tableauSansDoublons;
 }
 
-$monTableau = array(1, 1, 2, 2, 3, 4);
-$tableauSansDoublons = supprimerDoublons($monTableau);
+$langages = array("PHP", "Java", "PHP", "HTML", "CSS", "HTML");
+$unique = supprimerDoublons($langages);
 
-echo "Tableau sans doublons : ";
-foreach ($tableauSansDoublons as $element) {
-    echo $element . " ";
-}
-
-
-echo "Tableau sans doublons :\n";
-foreach ($tab as $value) {
-    echo $value . "\n";
-}
+print_r($unique);
 ?>
